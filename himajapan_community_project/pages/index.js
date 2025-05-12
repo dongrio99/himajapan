85110b596e0ef7a727665976fe460a7bbd575c83
dongrio99 <ehdgus2306@gmail.com>
2025-05-12 21:08:03 +0900

    Update index.js

--- himajapan_community_project/pages/index.js
+++ himajapan_community_project/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', padding: '100px' }}>
-      <h1>🚧 ひまJAPAN 오픈 준비 중입니다! 🚧</h1>
+      <h1>📢 ひまJAPAN 오픈 준비 중입니다！📢</h1>
       <p>이 페이지는 정상적으로 연결되었습니다.</p>
     </div>
   );
